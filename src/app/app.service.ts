91be7c2d4d530a7690d69df6919b6bbfdaff1a9c
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-11-06 00:15:12 -0500

    Updated AppService with Firebase CRUD functionality for Qwests

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestoreDocument,
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { config } from './app.config';
+import { Qwest } from './app.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  qwests: AngularFirestoreCollection<Qwest>;
 
-  constructor() { }
+  private qwestDoc: AngularFirestoreDocument<Qwest>;
+
+  constructor(private db: AngularFirestore) {
+    // Get the qwests collection
+    this.qwests = db.collection<Qwest>(config.collection_endpoint);
+  }
+
+  addQwest(qwest) {
+    //Add the new qwest to the collection
+    this.qwests.add(qwest);
+  }
+
+  updateQwest(id, update) {
+    // Get the qwest document
+    this.qwestDoc = this.db.doc<Qwest>(`${config.collection_endpoint}/${id}`);
+    // Update the qwest
+    this.qwestDoc.update(update);
+  }
+
+  deleteQwest(id) {
+    // Get the qwest document
+    this.qwestDoc = this.db.doc<Qwest>(`${config.collection_endpoint}/${id}`);
+    // Delete the qwest
+    this.qwestDoc.delete();
+  }
 }
